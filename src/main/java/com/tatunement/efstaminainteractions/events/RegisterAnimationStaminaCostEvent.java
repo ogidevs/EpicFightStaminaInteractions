d1fc178aba493cd75cace3760484d293ca2a8555
Tatunement <magmasevenchannel@gmail.com>
2025-02-03 14:01:40 +0100

    [1.1] Events now implement the IModBusEvent

--- src/main/java/com/tatunement/efstaminainteractions/events/RegisterAnimationStaminaCostEvent.java
+++ src/main/java/com/tatunement/efstaminainteractions/events/RegisterAnimationStaminaCostEvent.java
@@ -1,10 +1,11 @@
 package com.tatunement.efstaminainteractions.events;
 
 import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.fml.event.IModBusEvent;
 
 import java.util.Map;
 
-public class RegisterAnimationStaminaCostEvent extends Event {
+public class RegisterAnimationStaminaCostEvent extends Event implements IModBusEvent {
     private final Map<String, Float> animationStaminaCost;
 
     public RegisterAnimationStaminaCostEvent(Map<String, Float> animationStaminaCost) {
--- src/main/java/com/tatunement/efstaminainteractions/events/RegisterWeaponStaminaCostEvent.java
+++ src/main/java/com/tatunement/efstaminainteractions/events/RegisterWeaponStaminaCostEvent.java
@@ -1,11 +1,12 @@
 package com.tatunement.efstaminainteractions.events;
 
 import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.fml.event.IModBusEvent;
 import yesman.epicfight.world.capabilities.item.WeaponCategory;
 
 import java.util.Map;
 
-public class RegisterWeaponStaminaCostEvent extends Event {
+public class RegisterWeaponStaminaCostEvent extends Event implements IModBusEvent {
     private final Map<WeaponCategory, Float> weaponStaminaCostMap;
 
     public RegisterWeaponStaminaCostEvent(Map<WeaponCategory, Float> weaponStaminaCostMap) {
